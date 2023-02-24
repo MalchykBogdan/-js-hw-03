09bc1eb1dbbab9905d39d0c8a53fa95535bc2e7b
MalchykBogdan <124812110+MalchykBogdan@users.noreply.github.com>
2023-02-24 18:09:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+//Завдання Перше
+
+// const minNumerc = 1;
+// const maxNumeric = 10;
+// totalNumeric = 0;
+
+// for (let i = minNumerc; i <= maxNumeric; i+=1){
+//     const numeric = Math.round(
+//         Math.random() *(maxNumeric-minNumerc)+minNumerc
+//     );
+//     console.log(`Номер: ${i} - ${numeric}`);
+// }
+
+//Завдання Друге
+
+// let enterMood = prompt(`Введіть свій настрій`)
+ 
+// const mood = enterMood;
+// let number;
+
+// switch (mood){
+//     case "Щасливий":
+//         number = `🙂`;
+//         break;
+//     case "Сумний":
+//         number = `😢`;
+//         break;
+//     case "Злий":
+//         number = `😕`;
+//         break;
+// }
+// alert(number)
+
+//Завдання третє
+
+// let country = prompt('Оберіть країну для доставки:')
+// let lowercaseCountry = country.toLowerCase();
+
+// let price;
+
+// switch(lowercaseCountry){
+//     case "китай":
+//         price = `100`;
+//         break;
+//     case "чилі":
+//         price = `250`;
+//         break;
+//     case "індія":
+//         price = `80`;
+//         break;
+//     case "австралія":
+//         price = `170`;
+//         break;
+//     case "ямайка":
+//         price = `120`;
+//         break;
+//     default: alert('В вашу країні немає доставки')
+// }
+// alert(`Доставка в ${lowercaseCountry} буде коштувати ${price} кредитів`)
